1528d06b1b2a15842b35e3538cbeaac9027cda85
Suhail <suhailansari.sa354@gmail.com>
2025-10-30 00:11:41 +0530

    Added js code and css styling

--- To do list/script.js
+++ To do list/script.js
@@ -1,6 +1,26 @@
-function doSomething(x, y){
-    console.log(arguments)
-    arguments[1] = 20
-    return x+y
+// let ul = document.querySelector("ul");
+let btn = document.querySelector("#listTask");
+btn.addEventListener("click", addTask);
+
+
+function addTask(){
+    
+    let ul = document.querySelector("ul");
+    let input = document.querySelector("input");
+    let li = document.createElement("li");
+    let delBtn = document.createElement("button");
+
+
+    if(input.value!==""){
+
+    li.textContent=input.value;
+    delBtn.textContent = "remove";
+    li.appendChild(delBtn);
+    ul.appendChild(li);
+    input.value="";
+    delBtn.addEventListener("click", function(){
+        li.remove();
+    })
+
 }
-console.log(doSomething(5,10));
+}
